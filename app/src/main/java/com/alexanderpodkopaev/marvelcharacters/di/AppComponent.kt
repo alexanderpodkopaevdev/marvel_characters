b2309c8fd67600db8aa5de9c4a8a4b99dea2f403
Alexander Podkopaev <p-alex1990@mail.ru>
2021-04-27 10:02:33 +0500

    add dagger android

---
+++ app/src/main/java/com/alexanderpodkopaev/marvelcharacters/di/AppComponent.kt
@@ -0,0 +1,17 @@
+package com.alexanderpodkopaev.marvelcharacters.di
+
+import android.content.Context
+import com.alexanderpodkopaev.marvelcharacters.MarvelApp
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+
+@Component(modules = [AndroidInjectionModule::class])
+interface AppComponent : AndroidInjector<MarvelApp> {
+
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+}
